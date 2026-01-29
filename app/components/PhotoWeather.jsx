a680a0acf1aba1f8f62dab18434324f73ffa935a
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-01-29 14:58:25 -0300

    feat: previsão do tempo

---
+++ app/components/PhotoWeather.jsx
@@ -0,0 +1,100 @@
+import { Ionicons } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function PhotoWeather({ data, place }) {
+  if (!data || !place) {
+    return (
+      <BlurView intensity={80} tint="light" style={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Carregando informações…</Text>
+        </View>
+      </BlurView>
+    );
+  }
+
+  const { current, daily } = data;
+
+  return (
+    <BlurView intensity={80} tint="light" style={styles.container}>
+      {place ? (
+        <View style={styles.row}>
+          <Ionicons name="location-outline" size={18} color="white" />
+          <Text style={styles.text}>
+            {place.city}, {place.region} — {place.country}
+          </Text>
+        </View>
+      ) : (
+        <Text style={styles.text}>📍 Obtendo localização…</Text>
+      )}
+
+      <View style={styles.divider} />
+
+      {/* <View style={styles.row}>
+        <Ionicons name="camera-outline" size={18} color="white" />
+        <Text style={styles.text}>Condições agora</Text>
+      </View> */}
+      <View style={styles.row}>
+        <Ionicons name="thermometer-outline" size={18} color="white" />
+        <Text style={styles.text}>Temp: {current.temperature_2m}°C</Text>
+      </View>
+      <View style={styles.row}>
+        <Ionicons name="cloud-outline" size={18} color="white" />
+        <Text style={styles.text}>Nuvens: {current.cloud_cover}%</Text>
+      </View>
+      <View style={styles.row}>
+        <Ionicons name="speedometer-outline" size={18} color="white" />
+        <Text style={styles.text}>Vento: {current.wind_speed_10m} km/h</Text>
+      </View>
+      <View style={styles.row}>
+        <Ionicons name="rainy-outline" size={18} color="white" />
+        <Text style={styles.text}>Chuva: {current.precipitation} mm</Text>
+      </View>
+
+      <View style={styles.divider} />
+
+      {/* <View style={styles.row}>
+        <Ionicons name="sunny-outline" size={18} color="white" />
+        <Text style={styles.text}>Clima diário</Text>
+      </View> */}
+      {/* <View style={styles.row}>
+        <Ionicons name="sunny-outline" size={18} color="white" />
+        <Text style={styles.text}>Sol</Text>
+      </View> */}
+      <View style={styles.row}>
+        <Ionicons name="sunny-outline" size={18} color="white" />
+        <Text style={styles.text}>Nascer: {daily.sunrise[0]}</Text>
+      </View>
+      <View style={styles.row}>
+        <Ionicons name="moon-outline" size={18} color="white" />
+        <Text style={styles.text}>Pôr: {daily.sunset[0]}</Text>
+      </View>
+    </BlurView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 12,
+    alignSelf: "flex-start",
+    overflow: "hidden",
+    padding: 12,
+    backgroundColor: "#00000087",
+  },
+  text: {
+    color: "white",
+    marginBottom: 4,
+    fontSize: 16,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#e8e8e87b",
+    marginVertical: 12,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+  },
+});
--- app/components/TopBar.jsx
+++ app/components/TopBar.jsx
@@ -1,8 +1,12 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import { useState , useEffect,  } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Popover from "react-native-popover-view";
 import Animated from "react-native-reanimated";
 import useDeviceOrientation from "../hooks/useDeviceOrientation";
+import PhotoWeather from "./PhotoWeather";
+import * as Location from "expo-location"
 
 export default function TopBar({
   flash,
@@ -13,6 +17,56 @@ export default function TopBar({
 }) {
   const router = useRouter();
   const animatedStyle = useDeviceOrientation();
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState(null);
+  const [place, setPlace] = useState(null);
+  const [coords, setCoords] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.warn("Permissão de localização negada");
+        return;
+      }
+
+      const loc = await Location.getCurrentPositionAsync({});
+      setCoords({
+        lat: loc.coords.latitude,
+        lon: loc.coords.longitude,
+      });
+
+      console.log("Coords obtidas:", coords);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!coords) return;
+
+    fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&current=temperature_2m,cloud_cover,wind_speed_10m,precipitation&daily=sunrise,sunset&timezone=auto`,
+    )
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, [coords]);
+
+  useEffect(() => {
+    if (!coords) return;
+
+    fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.lat}&longitude=${coords.lon}&localityLanguage=pt`,
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setPlace({
+          city: json.city || json.locality || "Localização desconhecida",
+          region: json.principalSubdivision || "",
+          country: json.countryName || "",
+        });
+      })
+      .catch(console.error);
+  }, [coords]);
 
   return (
     <View style={styles.buttonsContainer}>
@@ -27,7 +81,7 @@ export default function TopBar({
         </Animated.View>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => toggleMode("zoom")}>
+      {/* <TouchableOpacity onPress={() => toggleMode("zoom")}>
         <Animated.View style={animatedStyle}>
           <Ionicons
             name="aperture-outline"
@@ -35,6 +89,24 @@ export default function TopBar({
             style={styles.button}
             color={activeControl === "zoom" ? "#ffaa00" : "white"}
           />
+        </Animated.View>
+      </TouchableOpacity> */}
+
+      <TouchableOpacity>
+        <Animated.View style={animatedStyle}>
+          <Popover
+            isVisible={open}
+            onRequestClose={() => setOpen(false)}
+            backgroundStyle={{ backgroundColor: "transparent" }}
+            popoverStyle={{ backgroundColor: "transparent" }}
+            from={
+              <TouchableOpacity onPress={() => setOpen(true)}>
+                <Ionicons name="cloud-outline" size={32} color="white" />
+              </TouchableOpacity>
+            }
+          >
+            <PhotoWeather data={data} place={place} />
+          </Popover>
         </Animated.View>
       </TouchableOpacity>
 
