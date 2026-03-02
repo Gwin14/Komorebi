e68b5390f0eae2e5d29320c175bb2f501db9f18f
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-03-02 15:20:06 -0300

    fix: modo pouca luz dando erro

--- app/components/CameraPreview.jsx
+++ app/components/CameraPreview.jsx
@@ -35,7 +35,6 @@ export default function CameraPreview({
         photo={true}
         video={false}
         audio={false}
-        lowLightBoost={true}
         zoom={zoom}
         onInitialized={onCameraReady}
       />
