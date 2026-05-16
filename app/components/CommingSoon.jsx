ab02aec01c70f1cd1deb374e08aa5b00289bde2d
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-05-16 11:03:44 -0300

    feat: atualização na lista de novidades

--- app/components/CommingSoon.jsx
+++ app/components/CommingSoon.jsx
@@ -7,30 +7,33 @@ import Backbutton from "../components/BackButton";
 export default function ComingSoon() {
   // Itens separados por seção
   const recentlyAdded = [
-    { id: "1", title: "Galeria imbutida" },
+    { id: "1", title: "Importação de LUTs personalizados" },
     { id: "2", title: "Metadados nas fotos" },
-    { id: "3", title: "Álbum criado automaticamente" },
-    { id: "4", title: "Previsão do tempo" },
-    { id: "5", title: "Filtros (alguns para teste)" },
-    { id: "6", title: "Vizualização dos dados da foto" },
-    { id: "7", title: "Flash" },
-    { id: "8", title: "Zoom" },
-    { id: "9", title: "Granulado em filtros" },
-    { id: "10", title: "Migração para sistema nativo de câmera" },
+    { id: "3", title: "Previsão do tempo" },
+    { id: "4", title: "Zoom com gesto de pinça" },
+    { id: "5", title: "Compensação de exposição" },
+    { id: "6", title: "Aspect ratio customizável" },
+    { id: "7", title: "Detecção de sorriso" },
+    { id: "8", title: "Migração para câmera nativa" },
+    { id: "9", title: "Salvar original junto da foto com LUT" },
+    { id: "10", title: "Galeria melhorada" },
   ];
 
   const comingSoon = [
-    { id: "11", title: "Feedback háptil configurável" },
-    { id: "12", title: "Mais opções de aspect ratio" },
-    { id: "13", title: "Importação de fitros" },
-    { id: "14", title: "Timer pra fotos" },
+    { id: "11", title: "Configuração do feedback háptil" },
+    { id: "12", title: "Timer para fotos" },
+    { id: "13", title: "Grid customizável" },
+    { id: "14", title: "Indicador de zoom" },
+    { id: "15", title: "Formato de arquivo" },
+    { id: "16", title: "Desativar álbum automático" },
+    { id: "17", title: "HDR" },
   ];
 
   const whoKnows = [
-    { id: "15", title: "Widget (IOS)" },
-    { id: "16", title: "Gravação de vídeo" },
-    { id: "17", title: "Controles manuais" },
-    { id: "18", title: "Raw" },
+    { id: "18", title: "Widget com sugestões baseadas no clima" },
+    { id: "19", title: "Controles manuais" },
+    { id: "20", title: "RAW" },
+    { id: "21", title: "Sistema de conquistas" },
   ];
 
   // renderItem agora recebe IconComponent e iconName como parâmetros
